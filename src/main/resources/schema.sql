6152baa8f2647646aa67a656c25f89dd15a23724
setoguchi <setoguchi.takuma@gmail.com>
2023-01-27 12:00:06 +0900

    sql追加

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,61 @@
+CREATE SCHEMA 商品;
+CREATE TABLE 商品.商品
+(
+    商品コード VARCHAR(10) NOT NULL,
+    商品名称 VARCHAR(20) NOT NULL,
+    商品単価 NUMERIC(9, 3) NOT NULL,
+    PRIMARY KEY (商品コード),
+    作成日時 TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE SCHEMA 顧客;
+CREATE TABLE 顧客.顧客区分
+(
+    個人法人区分 VARCHAR(2) NOT NULL PRIMARY KEY,
+    作成日時 TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE 顧客.顧客
+(
+    顧客番号 UUID NOT NULL,
+    顧客名称 VARCHAR(20) NOT NULL,
+    個人法人区分 CHARACTER(2) NOT NULL,
+    PRIMARY KEY (顧客番号),
+    FOREIGN KEY (個人法人区分) REFERENCES 顧客.顧客区分 (個人法人区分),
+    作成日時 TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE SCHEMA 受注;
+CREATE TABLE 受注.受注
+(
+    受注番号 UUID NOT NULL,
+    顧客番号 UUID NOT NULL,
+    PRIMARY KEY (受注番号),
+    FOREIGN KEY (顧客番号) REFERENCES 顧客.顧客 (顧客番号),
+    作成日時 TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE 受注.受注明細
+(
+    受注番号 UUID NOT NULL,
+    受注明細番号 UUID NOT NULL,
+    商品コード VARCHAR(10) NOT NULL,
+    受注数量 NUMERIC(3) NOT NULL,
+    PRIMARY KEY (受注番号, 受注明細番号),
+    FOREIGN KEY (受注番号) REFERENCES 受注.受注 (受注番号),
+    FOREIGN KEY (商品コード) REFERENCES 商品.商品 (商品コード),
+    作成日時 TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE SCHEMA 引当;
+CREATE TABLE 引当.引当
+(
+    引当番号 UUID NOT NULL,
+    受注番号 UUID NOT NULL,
+    受注明細番号 UUID NOT NULL,
+    引当数量 NUMERIC(3) NOT NULL,
+    PRIMARY KEY (引当番号),
+    FOREIGN KEY (受注番号, 受注明細番号) REFERENCES 受注.受注明細 (受注番号, 受注明細番号),
+    作成日時 TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
---
+++ src/test/java/guide/tm/JigErd.java
@@ -0,0 +1,23 @@
+package guide.tm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.util.Map;
+
+@SpringBootTest
+public class JigErd {
+    @Test
+    void run(@Autowired DataSource dataSource) {
+        jig.erd.JigErd.run(dataSource, Map.of(
+                "jig.erd.output.directory", "./build-jig-erd",
+                "jig.erd.output.format", "svg",
+                "jig.erd.output.rankdir", "RL",
+                "jig.erd.dot.custom.区分.label-pattern", ".+区分",
+                "jig.erd.dot.custom.区分.shape", "parallelogram",
+                "jig.erd.dot.custom.区分.fillcolor", "yellow"
+        ));
+    }
+}
