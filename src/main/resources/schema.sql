2ef5cae18e205c6cca62f0ffccbcb377a4d4581b
setoguchi <setoguchi.takuma@gmail.com>
2023-03-24 12:14:15 +0900

    カラム名変更
    - 顧客番号 → 顧客ID
    - 受注番号 → 受注ID

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -55,13 +55,13 @@ CREATE TABLE 顧客.顧客区分
 
 CREATE TABLE 顧客.顧客
 (
-    顧客番号 UUID NOT NULL,
+    顧客ID UUID NOT NULL,
     顧客姓 VARCHAR(20) NOT NULL,
     顧客名 VARCHAR(20) NOT NULL,
     顧客姓カナ VARCHAR(20) NOT NULL,
     顧客名カナ VARCHAR(20) NOT NULL,
     個人法人区分 CHARACTER(2) NOT NULL,
-    PRIMARY KEY (顧客番号),
+    PRIMARY KEY (顧客ID),
     FOREIGN KEY (個人法人区分) REFERENCES 顧客.顧客区分 (個人法人区分),
     作成日時 TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
@@ -72,20 +72,20 @@ CREATE TABLE 顧客.顧客住所
     都道府県 VARCHAR(20) NOT NULL,
     住所 VARCHAR(40) NOT NULL,
     PRIMARY KEY (顧客番号),
-    FOREIGN KEY (顧客番号) REFERENCES 顧客.顧客 (顧客番号),
+    FOREIGN KEY (顧客番号) REFERENCES 顧客.顧客 (顧客ID),
     作成日時 TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE SCHEMA 受注;
 CREATE TABLE 受注.受注
 (
-    受注番号 UUID NOT NULL,
-    顧客番号 UUID NOT NULL,
+    受注ID UUID NOT NULL,
+    顧客ID UUID NOT NULL,
     受注日 DATE NOT NULL,
     都道府県 VARCHAR(5) NOT NULL,
     住所 VARCHAR(50) NOT NULL,
-    PRIMARY KEY (受注番号),
-    FOREIGN KEY (顧客番号) REFERENCES 顧客.顧客 (顧客番号),
+    PRIMARY KEY (受注ID),
+    FOREIGN KEY (顧客ID) REFERENCES 顧客.顧客 (顧客ID),
     作成日時 TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
@@ -96,7 +96,7 @@ CREATE TABLE 受注.受注明細
     商品コード VARCHAR(10) NOT NULL,
     受注数量 NUMERIC(3) NOT NULL,
     PRIMARY KEY (受注番号, 受注明細番号),
-    FOREIGN KEY (受注番号) REFERENCES 受注.受注 (受注番号),
+    FOREIGN KEY (受注番号) REFERENCES 受注.受注 (受注ID),
     FOREIGN KEY (商品コード) REFERENCES 商品.商品 (商品コード),
     作成日時 TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
@@ -108,7 +108,7 @@ CREATE TABLE 受注.セット商品受注明細
     商品コード VARCHAR(10) NOT NULL,
     受注数量 NUMERIC(3) NOT NULL,
     PRIMARY KEY (受注番号, 受注明細番号),
-    FOREIGN KEY (受注番号) REFERENCES 受注.受注 (受注番号),
+    FOREIGN KEY (受注番号) REFERENCES 受注.受注 (受注ID),
     FOREIGN KEY (商品コード) REFERENCES 商品.セット商品 (商品コード),
     作成日時 TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
@@ -117,7 +117,7 @@ CREATE TABLE 受注.消費税計算方式
 (
     受注番号 UUID NOT NULL,
     計算方式 CHAR(4) NOT NULL,
-    FOREIGN KEY (受注番号) REFERENCES 受注.受注 (受注番号),
+    FOREIGN KEY (受注番号) REFERENCES 受注.受注 (受注ID),
     FOREIGN KEY (計算方式) REFERENCES 消費税.計算方式区分 (消費税計算方式),
     作成日時 TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
@@ -203,7 +203,7 @@ CREATE TABLE 出荷指示.出荷指示
     出荷日 DATE NOT NULL,
     受注番号 UUID NOT NULL,
     PRIMARY KEY (出荷番号),
-    FOREIGN KEY (受注番号) REFERENCES 受注.受注 (受注番号),
+    FOREIGN KEY (受注番号) REFERENCES 受注.受注 (受注ID),
     作成日時 TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
